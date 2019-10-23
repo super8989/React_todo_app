e6c37b6562d9ded95301964f4c1d763ccd6e3014
Sam <super8989@gmail.com>
2019-10-22 22:15:38 -0400

    Edit input is now a controlled input which can edit the todo item

--- src/Todo.js
+++ src/Todo.js
@@ -4,10 +4,12 @@ class Todo extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isEditing: false
+			isEditing: false,
+			task: ""
 		};
 		this.handleRemove = this.handleRemove.bind(this);
 		this.toggleForm = this.toggleForm.bind(this);
+		this.handleChange = this.handleChange.bind(this);
 	}
 
 	handleRemove() {
@@ -18,14 +20,31 @@ class Todo extends Component {
 		this.setState({ isEditing: !this.state.isEditing });
 	}
 
+	handleUpdate(evt) {
+		evt.preventDefault();
+		//take new task data and pass up to parent
+	}
+
+	handleChange(evt) {
+		this.setState({
+			[evt.target.name]: evt.target.value
+		});
+	}
+
 	render() {
 		let result;
 
 		if (this.state.isEditing) {
 			result = (
 				<div>
-					<form>
-						<input type='text' />
+					<form onSubmit={this.handleUpdate}>
+						<input
+							type='text'
+							value={this.state.task}
+							name='task'
+							onChange={this.handleChange}
+						/>
+						<button>Save</button>
 					</form>
 				</div>
 			);
