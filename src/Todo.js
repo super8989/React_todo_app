0da5da568ce2f22d7e572d7b7b410f1408d9daf4
Sam <super8989@gmail.com>
2019-10-22 22:10:31 -0400

    added toggleForm to button onClick to toggle between true and false for editing the todo item

--- src/Todo.js
+++ src/Todo.js
@@ -7,10 +7,15 @@ class Todo extends Component {
 			isEditing: false
 		};
 		this.handleRemove = this.handleRemove.bind(this);
+		this.toggleForm = this.toggleForm.bind(this);
 	}
 
 	handleRemove() {
 		this.props.removeTodo(this.props.id);
+	}
+
+	toggleForm() {
+		this.setState({ isEditing: !this.state.isEditing });
 	}
 
 	render() {
@@ -27,7 +32,7 @@ class Todo extends Component {
 		} else {
 			result = (
 				<div>
-					<button>Edit</button>
+					<button onClick={this.toggleForm}>Edit</button>
 					<button onClick={this.handleRemove}>X</button>
 					<li>{this.props.task}</li>
 				</div>
