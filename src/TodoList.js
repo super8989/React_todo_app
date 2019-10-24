ec892553829847423dbfce4e1f7ec040e6202bc8
Sam <super8989@gmail.com>
2019-10-23 20:26:03 -0400

    toggling todo completion to mark the task completed

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends Component {
 		this.create = this.create.bind(this);
 		this.remove = this.remove.bind(this);
 		this.update = this.update.bind(this);
+		this.toggleCompletion = this.toggleCompletion.bind(this);
 	}
 
 	create(newTodo) {
@@ -35,6 +36,16 @@ class TodoList extends Component {
 		this.setState({ todos: updatedTodos });
 	}
 
+	toggleCompletion(id) {
+		const updatedTodos = this.state.todos.map(todo => {
+			if (todo.id === id) {
+				return { ...todo, completed: !todo.completed };
+			}
+			return todo;
+		});
+		this.setState({ todos: updatedTodos });
+	}
+
 	render() {
 		const todos = this.state.todos.map(todo => {
 			return (
@@ -42,8 +53,10 @@ class TodoList extends Component {
 					key={todo.id}
 					id={todo.id}
 					task={todo.task}
+					completed={todo.completed}
 					removeTodo={this.remove}
 					updateTodo={this.update}
+					toggleTodo={this.toggleCompletion}
 				/>
 			);
 		});
